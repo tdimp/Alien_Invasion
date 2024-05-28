8dd336bd1be19d1b637991c809be9e5e9167274c
Thomas Dimperio <twdimperio@gmail.com>
2024-05-28 19:54:38 +0200

    Refactor to split event handling to separate function

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,24 +19,29 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
         
+        self.ship = Ship(self)
+        
         # Set the background color.
         self.bg_color = (230, 230, 230)
         
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             # Make the most recently drawn screen visible.abs
             pygame.display.flip()
             self.clock.tick(60)
-            
+    
+    def _check_events(self):
+         # Respond to keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
 if __name__ == '__main__':
     # Make a game instance and run the game
