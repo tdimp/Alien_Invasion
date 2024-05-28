45d9e01a6f7db6dfaa26a3aecf1b8b2f99f4e4eb
Thomas Dimperio <twdimperio@gmail.com>
2024-05-28 19:58:00 +0200

    Refactor to move update screen logic into helper function

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,20 +28,21 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible.abs
-            pygame.display.flip()
+            self._update_screen()
             self.clock.tick(60)
     
     def _check_events(self):
-         # Respond to keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        """Respond to keyboard and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+            
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance and run the game
